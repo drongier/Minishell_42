a41a802371f6ec173a7301e086c95799374c1952
cchakibb <chbachir@student.42berlin.de>
2024-12-03 14:25:36 +0100

    export shortened and norminetted

--- src/bin/export.c
+++ src/bin/export.c
@@ -6,64 +6,72 @@
 /*   By: chbachir <chbachir@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 21:32:10 by emaydogd          #+#    #+#             */
-/*   Updated: 2024/11/26 14:46:57 by chbachir         ###   ########.fr       */
+/*   Updated: 2024/12/03 14:22:57 by chbachir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int is_valid_key(char *key)
+static int	is_valid_key(t_shell *shell, char *key)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (ft_isalpha(key[i]) == 0 && key[i] != '_')
+	{
+		ft_error(shell, "bash: export: `%s': not a valid identifier\n", \
+		(char *)shell->parser->args->content, 1);
 		return (0);
+	}
 	i++;
 	while (key[i])
 	{
 		if (ft_isalnum(key[i]) == 0 && key[i] != '_')
+		{
+			ft_error(shell, "bash: export: `%s': not a valid identifier\n", \
+			(char *)shell->parser->args->content, 1);
 			return (0);
+		}
 		i++;
 	}
 	return (1);
 }
 
+static char	*remove_quotes_export(char *val)
+{
+	size_t	len;
+	char	*new_val;
+
+	len = ft_strlen(val);
+	if ((val[0] == '"' || val[0] == '\'') && val[len - 1] == val[0])
+	{
+		new_val = ft_substr(val, 1, len - 2);
+		return (new_val);
+	}
+	return (val);
+}
+
 void	exec_export(t_shell *shell)
 {
 	int		i;
-	int		j;
 	char	*key;
 	char	*val;
 
-	i = 0;
-	while(shell->parser->args)
+	while (shell->parser->args)
 	{
-		j = 0;
-		while (((char *)shell->parser->args->content)[j])
+		i = 0;
+		while (((char *)shell->parser->args->content)[i])
 		{
-			if (((char *)shell->parser->args->content)[j] == '=')
+			if (((char *)shell->parser->args->content)[i] == '=')
 				break ;
-			j++;
+			i++;
 		}
-		key = ft_substr(shell->parser->args->content, 0, j);
-		if (!is_valid_key(key))
-		{
-			ft_error(shell, "bash: export: `%s': not a valid identifier\n", (char *)shell->parser->args->content, 1);
+		key = ft_substr(shell->parser->args->content, 0, i);
+		if (!is_valid_key(shell, key))
 			break ;
-			//exit(EXIT_FAILURE);
-		}
-		val = ft_substr(shell->parser->args->content, j + 1, ft_strlen(shell->parser->args->content) - j - 1); // Correction ici
-		// Supprimer les guillemets si présents
-		if (val[0] == '"' || val[0] == '\'')
-		{
-			size_t len = ft_strlen(val);
-			if (val[len - 1] == val[0])
-			{
-				val[len - 1] = '\0';
-				val = ft_substr(val, 1, len - 2);
-			}
-		}
+		val = ft_substr(shell->parser->args->content, i + 1, \
+		ft_strlen(shell->parser->args->content) - i - 1);
+		val = remove_quotes_export(val);
 		if (ft_getenv(shell, key))
 			env_pop(&shell->env, key);
 		if (val[0])
@@ -71,35 +79,3 @@ void	exec_export(t_shell *shell)
 		shell->parser->args = shell->parser->args->next;
 	}
 }
-/* void	exec_export(t_shell *shell)
-{
-	int		i;
-	int		j;
-	char	*key;
-	char	*val;
-
-	i = 0;
-	while(shell->parser->args)
-	{
-		j = 0;
-		while (((char *)shell->parser->args->content)[j])
-		{
-			if (((char *)shell->parser->args->content)[j] == '=')
-				break ;
-			j++;
-		}
-		key = ft_substr(shell->parser->args->content, 0, j);
-		if (!is_valid_key(key))
-		{
-			ft_error(shell, "bash: export: `%s': not a valid identifier\n", (char *)shell->parser->args->content);
-			break ;
-		}
-		val = ft_substr(shell->parser->args->content, j + 1, ft_strlen(shell->parser->args->content) - j + 1);
-		if (ft_getenv(shell, key))
-			env_pop(&shell->env, key);
-		if (val[0])
-			env_push(&shell->env, key, val);
-		shell->parser->args = shell->parser->args->next;
-		ft_error(shell, NULL, NULL);
-	}
-} */
