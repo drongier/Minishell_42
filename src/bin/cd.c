94d35ea7d386c86b1d1d695b06653aed3700e373
Drongier <rongier.damien@gmail.com>
2024-12-13 20:03:26 +0100

    fixe cd

--- src/bin/cd.c
+++ src/bin/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chbachir <chbachir@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: drongier <drongier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 21:27:23 by emaydogd          #+#    #+#             */
-/*   Updated: 2024/12/10 11:13:38 by chbachir         ###   ########.fr       */
+/*   Updated: 2024/12/13 20:02:20 by drongier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,28 @@
 void	exec_cd(t_shell *shell, t_list *args)
 {
 	char	*home;
+	char	*clean_arg;
 
 	home = ft_getenv(shell, "HOME");
 	if (home == NULL)
 		return (ft_error(shell, "bash: cd: HOME not set\n", NULL, 1));
 	if (ft_lstsize(args) >= 2)
 		return (ft_error(shell, "bash: cd: too many arguments\n", NULL, 1));
-	if (args == NULL || args->content == NULL)
+	clean_arg = remove_quotes((char *)args->content);
+	if (args == NULL || clean_arg == NULL)
 	{
 		if (chdir(home) != 0)
 			ft_error(shell, "bash: cd: %s: "NSFOD"\n", home, 1);
+		free(clean_arg);
 		return ;
 	}
-	if (ft_strncmp((char *)args->content, "~", 1) == 0)
+	if (ft_strncmp(clean_arg, "~", 1) == 0)
 	{
 		if (chdir(home) != 0)
 			ft_error(shell, "bash: cd: %s: "NSFOD"\n", home, 1);
-		return ;
+		return (free(clean_arg));
 	}
-	else if (chdir((char *)args->content) != 0)
-		ft_error(shell, "bash: cd: %s: "NSFOD"\n", (char *)args->content, 1);
+	else if (chdir(clean_arg) != 0)
+		ft_error(shell, "bash: cd: %s: "NSFOD"\n", clean_arg, 1);
+	free(clean_arg);
 }
